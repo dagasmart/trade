de19b7dae809afe1fa7ea4002e2f1dd63f62a182
raoxikui <raoxikui@qq.com>
2025-08-14 14:05:20 +0800

    优化

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,17 +1,15 @@
 <?php
 
-use DagaSmart\BizAdmin\Middleware\Authenticate;
-use DagaSmart\BizAdmin\Middleware\Permission;
 use Illuminate\Routing\Router;
-use DagaSmart\Trade\Http\Controllers;
 use Illuminate\Support\Facades\Route;
+use DagaSmart\Trade\Http\Controllers;
+use DagaSmart\BizAdmin\Middleware\Permission;
+use DagaSmart\BizAdmin\Middleware\Authenticate;
 
 
 //需登录与鉴权
 Route::group([
-    'domain'     => config('admin.route.domain'),
     'prefix'     => 'trade',
-    'middleware' => [Authenticate::class],
 ], function (Router $router) {
     //交易流水
     $router->resource('record', Controllers\RecordController::class);
@@ -21,19 +19,15 @@ Route::group([
     $router->resource('stat', Controllers\StatController::class);
     //支付设置
     $router->resource('settings', Controllers\SettingController::class);
-    //识别终端/生成订单
-    Route::get('payment/detect/{source}/{order_no}', [Controllers\PaymentController::class, 'detect'])->withoutMiddleware([Authenticate::class, Permission::class]);
     //订单付款
-    Route::get('payment/order/{order_no}', [Controllers\PaymentController::class, 'order'])->withoutMiddleware([Authenticate::class, Permission::class]);
+    $router->get('payment/order/{order_no}', [Controllers\PaymentController::class, 'order']);
 });
 
 //免登录无限制
 Route::group([
-    'domain'     => config('admin.route.domain'),
     'prefix'     => 'trade',
 ], function (Router $router) {
-    $router->get('_iconify_search', [\DagaSmart\BizAdmin\Controllers\IndexController::class, 'iconifySearch']);
-
-    Route::get('trade', [Controllers\TradeController::class, 'index']);
+    //识别终端/生成订单
+    $router->get('payment/detect/{source}/{order_no}', [Controllers\PaymentController::class, 'detect'])->withoutMiddleware([Authenticate::class, Permission::class]);
 
 });
