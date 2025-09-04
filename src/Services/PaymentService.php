eec062423d18e2a22c450eb78c45ebeba5da0e5e
raoxikui <raoxikui@qq.com>
2025-09-05 00:02:35 +0800

    1

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -7,6 +7,7 @@ use DagaSmart\Trade\Models\Payment;
 use DagaSmart\Trade\Models\Order;
 use DagaSmart\Trade\Models\Record;
 use ErrorException;
+use Illuminate\Http\RedirectResponse;
 use Psr\Http\Message\ResponseInterface;
 use Yansongda\Artful\Exception\ContainerException;
 use Yansongda\Artful\Rocket;
@@ -24,10 +25,10 @@ class PaymentService extends AdminService
 
     /**
      * @param $data
-     * @return ResponseInterface|true|Rocket|Collection
+     * @return RedirectResponse|ResponseInterface|true|Rocket|Collection
      * @throws ErrorException
      */
-    public function payOrder($data): true|Collection|ResponseInterface|Rocket
+    public function payOrder($data)
     {
         $source = $data['source'] ?? null;
         if (!$source) {
@@ -113,7 +114,8 @@ class PaymentService extends AdminService
             ->whereNotIn('trade_status',[0]) //排除待付状态
             ->exists();
         if($exists){
-            throw new ErrorException($trade_channel_as . '交易订单已付款，请勿重复');
+            //return throw new ErrorException($trade_channel_as . '交易订单已付款，请勿重复');
+            return back()->withErrors($trade_channel_as . '交易订单已付款，请勿重复');
         }
         $record = $model->query()->updateOrCreate(
             // 查找条件，如果找不到，则按这些条件创建新记录，并更新这些字段的值
