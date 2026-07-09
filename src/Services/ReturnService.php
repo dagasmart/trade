c15ef01a9a8cb4adf0485adb4604904f6eda70be
raoxikui <raoxikui@qq.com>
2026-07-09 22:23:22 +0800

    优化

--- src/Services/ReturnService.php
+++ src/Services/ReturnService.php
@@ -2,31 +2,29 @@
 
 namespace DagaSmart\Trade\Services;
 
-use DagaSmart\Trade\Models\Payment;
 use DagaSmart\Trade\Models\Order;
 use DagaSmart\Trade\Models\Record;
 use ErrorException;
 use phpDocumentor\Reflection\DocBlock\Tags\Throws;
-
 
 /**
  * 支付同步回调类
  */
 class ReturnService extends AdminService
 {
-
     /**
      * @throws ErrorException
      */
     public function paySave($data)
     {
-        //throw new ErrorException('我是扣不');
+        // throw new ErrorException('我是扣不');
         $model = new Order;
+
         return admin_transaction(function () use (&$model, $data) {
 
             $row = $model->query()
                 ->where(['order_no' => $data->out_trade_no])
-                //->where(['trade_status' => 0])
+                // ->where(['trade_status' => 0])
                 ->first();
             if ($row) {
                 if ($row->trade_status == 1) {
@@ -34,34 +32,32 @@ class ReturnService extends AdminService
                 }
                 $row->trade_no = $data->trade_no;
                 $row->trade_amount = $data->total_amount;
-                $row->trade_status = 1; //支付成功
+                $row->trade_status = 1; // 支付成功
                 $row->trade_time = $data->timestamp;
                 if ($row->save()) {
 
-                    //记录流水
+                    // 记录流水
                     $log = [
                         'trade_id' => $row->id,
                         'order_no' => $row->order_no,
-                        'trade_type' => in_array($row->trade_status, [0,1]) ? 1 : 2,
+                        'trade_type' => in_array($row->trade_status, [0, 1]) ? 1 : 2,
                         'trade_channel' => $row->trade_channel,
                         'trade_amount' => $row->trade_amount,
                         'trade_status' => $row->trade_status,
                         'trade_code' => $row->trade_code,
                         'trade_result' => $row->trade_result,
-                        'opera_type' => in_array($row->trade_status, [0,1]) ? 'user' : (admin_mer_id() ? 'mer' : 'plat'),
+                        'opera_type' => in_array($row->trade_status, [0, 1]) ? 'user' : (admin_mer_id() ? 'mer' : 'plat'),
                         'opera_id' => $row->payer_id,
                         'opera' => $row->payer,
                     ];
                     $model = new Record;
                     $model->query()->insert($log);
 
-
-                    //admin_order_feedback($row);
+                    // admin_order_feedback($row);
                 }
             }
+
             return false;
         });
     }
-
-
 }
--- src/helpers.php
+++ src/helpers.php
@@ -2,11 +2,9 @@
 
 use DagaSmart\Trade\TradeServiceProvider;
 
-if (!function_exists('trade_pay_config')) {
+if (! function_exists('trade_pay_config')) {
     /**
      * 获取支付配置参数
-     * @param array $data
-     * @return array
      */
     function trade_pay_config(array $data = []): array
     {
@@ -14,9 +12,7 @@ if (!function_exists('trade_pay_config')) {
     }
 }
 
-
-
-if (!function_exists('trade_order_sn')) {
+if (! function_exists('trade_order_sn')) {
     /**
      * 微秒时间戳转换日期序列生成交易订单号
      */
@@ -24,18 +20,17 @@ if (!function_exists('trade_order_sn')) {
     {
         $microtime = $microtime ?? microtime(true);
         $channel = $channel ? strtoupper($channel) : null;
-        return $channel . date('YmdHis' . str_replace('.', '', fmod($microtime,1)), $microtime);
+
+        return $channel.date('YmdHis'.str_replace('.', '', fmod($microtime, 1)), $microtime);
     }
 }
 
-if (!function_exists('admin_trade_trans')) {
+if (! function_exists('admin_trade_trans')) {
     /**
      * 语言包
-     * @param $key
-     * @return array|string|null
      */
     function admin_trade_trans($key): array|string|null
     {
-        return TradeServiceProvider::trans('trade.' . $key) ?? null;
+        return TradeServiceProvider::trans('trade.'.$key) ?? null;
     }
 }
